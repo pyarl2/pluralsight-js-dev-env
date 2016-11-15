237f0f0afafcfc38e803548f1b9119589f7c915a
Paul Yarl <pyarl2@gmail.com>
2016-11-15 08:53:29 -0600

    refactor es6 features like const

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,9 +2,9 @@ import express from "express";
 import path from "path";
 import open from "open";
 
-var port = 3000;
+const port = 3000;
 
-var app = express();
+const app = express();
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
